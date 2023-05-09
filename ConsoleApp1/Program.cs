171a60fcfe7c17eda1df5d3ba7f3bc84311ba36c
facundoacostayl <facundoacostayl@outlook.com>
2023-05-09 07:38:04 -0300

    NumberConverter class methods added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -69,14 +69,107 @@ namespace ConsoleApp1
 
         public class NumberConverter
         {
-            private int temp;
-            private int userNumberAsStringLength;
-            private int value;
+            private int currentDigit;
+            private int numberStrLength;
+            private int numberStrLengthElevated;
+
+            public void setCurrentDigit(int number, int numberElevated) { this.currentDigit = number / numberElevated; }
+            public void SetNumberStrLength(int number) { this.numberStrLength = number.ToString().Length; }
+
+            public void SetNumberStrLengthElevated(int numberLength) { this.numberStrLengthElevated = (int)Math.Pow(10, numberLength - 1); }
+
+            public int GetNumberStrLength() { return this.numberStrLength; }
+            public int GetCurrentDigit() { return this.currentDigit; }
+            public int GetNumberStrLengthElevated() { return this.numberStrLengthElevated; }
+
+            
         }
 
 
         public static void Main(string[] args)
         {
+            Number numberList = new Number();
+            NumberConverter numberConverter = new NumberConverter();
+            User user = new User();
+
+            Console.WriteLine("Enter a number (Max: 9999)");
+
+            user.SetNumber(Int32.Parse(Console.ReadLine()));
+            int userNumber = user.GetNumber();
+
+            if (userNumber == 0)
+            {
+                Console.WriteLine(numberList.GetCentenasYMiles()[0]);
+                Console.ReadLine();
+                return;
+            }
+
+            if (userNumber < 0 || userNumber > 9999)
+            {
+                Console.WriteLine("Number should be less than 9999 and greater than 0");
+                Console.ReadLine();
+                return;
+            }
+
+            Boolean flag = false;
+            while (userNumber >= 0)
+            {
+                userNumberAsStringLength = userNumber.ToString().Length;
+                if (userNumber > 0)
+                {
+
+                    value = (int)Math.Pow(10, userNumberAsStringLength - 1);
+                    temp = userNumber / value;
+
+                }
+
+                switch (userNumberAsStringLength)
+                {
+                    case 1:
+                        Console.Write(" " + unidades[temp - 1] + " ");
+
+                        break;
+                    case 2:
+                        if (userNumber >= 10 && userNumber < 20)
+                        {
+                            userNumber = userNumber % value;
+
+                            Console.Write(" " + decenasA[userNumber] + " ");
+
+                            flag = true;
+
+                        }
+                        else
+                        {
+                            Console.Write(" " + decenasB[temp - 1] + " ");
+                        }
+
+                        break;
+                    case 3:
+                        Console.Write(unidades[temp - 1] + " " + centenasYMiles[1]);
+                        break;
+                    case 4:
+                        Console.Write(" " + unidades[temp - 1] + " " + centenasYMiles[2] + " ");
+                        break;
+                }
+
+
+                if (userNumber == value)
+                {
+                    Console.Write(" ");
+                    break;
+                }
+                else
+                {
+                    userNumber = userNumber % value;
+                }
+
+                if (userNumber == 0 || flag == true)
+                {
+                    break;
+                }
+
+            }
 
         }
     }
